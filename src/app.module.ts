975155bef86442dba1deb8353362718238b41c0d
Oskar Barcz <oskar.barcz@contractor.atolls.com>
2026-08-17 17:34:12 +0200

    Rebrand and change domain CORS settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { StatusController } from 'modules/status/status.controller';
+import { StatusController } from './modules/status/status.controller';
 import { CacheModule } from '@nestjs/cache-manager';
 import { AuthModule } from './modules/auth/auth.module';
 import { PositionModule } from './modules/position/position.module';
